d86a413919d90584d5ac86864191255b8204cfd6
Emil Sandberg <emil.hietanen@gmail.com>
2018-04-19 17:30:34 +0200

    Removed comment related files for test purposes

--- view/comment/vote-buttons.php
+++
@@ -1,8 +0,0 @@
-<?php
-$actionUrl = $this->url("comment/{$comment->post_id}/vote?id={$comment->id}");
-?>
-
-<form class='vote-buttons' method="post" action="<?= $actionUrl ?>">
-    <input type="submit" name="upvote" value="&uarr;"><br>
-    <input type="submit" name="downvote" value="&darr;">
-</form>
